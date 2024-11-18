03babe4ba8628e8d1dd76bb00f120a3e95cc42a8
steven <bobbobston252@gmail.com>
2024-11-17 20:43:18 -0600

    added code for functioning spinners from activity_edit.xml

--- app/src/main/java/com/cs407/groupproject407/editActivity.kt
+++ app/src/main/java/com/cs407/groupproject407/editActivity.kt
@@ -22,32 +22,88 @@ class editActivity : AppCompatActivity() {
             insets
         }
 
+        //code for spinner activityType
         val spinner: Spinner = findViewById(R.id.activityType)
-
         // Create a list of two options
         val options = listOf("", "Recreation", "Work")
-
         // Create an ArrayAdapter to populate the Spinner with the options
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
         // Set the adapter to the Spinner
         spinner.adapter = adapter
         var selected: String
-
         // Set an item selected listener to respond to user selection
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 // Get the selected option
                 selected = parent?.getItemAtPosition(position).toString()
-                // Display a toast message with the selected option
                 Log.i("dfd",selected)
-
             }
-
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 // Handle case where nothing is selected (optional)
             }
         }
+
+        //code for monthSpinner
+        //all spinners have almost exactly the same code except different var names and array of choices
+        val monthSpinner: Spinner = findViewById(R.id.chooseMonth)
+        val monthOptions = listOf("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+        val monthAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, monthOptions)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        monthSpinner.adapter = monthAdapter
+        var monthSelected: String
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                monthSelected = parent?.getItemAtPosition(position).toString()
+
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Handle case where nothing is selected (optional)
+            }
+        }
+
+        //code for chooseDay
+        //TODO: how many days to choose should vary by what month you choose
+        val daySpinner: Spinner = findViewById(R.id.chooseDay)
+        val dayOptions = (1..31).toList()
+        val dayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, dayOptions)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        daySpinner.adapter = dayAdapter
+        var daySelected: String
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                daySelected = parent?.getItemAtPosition(position).toString()
+
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Handle case where nothing is selected (optional)
+            }
+        }
+
+        //code for chooseYear
+        //TODO: spinner should start with current year
+        val yearSpinner: Spinner = findViewById(R.id.chooseYear)
+        val yearOptions = (2024..2050).toList()
+        val yearAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, yearOptions)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        yearSpinner.adapter = yearAdapter
+        var yearSelected: String
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                yearSelected = parent?.getItemAtPosition(position).toString()
+
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Handle case where nothing is selected (optional)
+            }
+        }
+
+
+
+
+
+
+
+
     }
 }
